fd6599534713b68d66eac8e7496072108ed0bdc4
Aleksandra Zieniewicz <olllenka.zie@gmail.com>
2020-02-22 13:54:54 +0100

    custom exceptions extends RuntimeException, exceptions are handled in try/catch in PersonTester

--- src/PersonTester.java
+++ src/PersonTester.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class PersonTester {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Person person;
+
         System.out.println("Podaj imię");
         String name = sc.nextLine();
         System.out.println("Podaj nazwisko");
@@ -11,10 +11,11 @@ public class PersonTester {
         System.out.println("Podaj wiek");
         int age = sc.nextInt();
 
-        person = new Person(name, lastName, age);
-
-        System.out.println(person.toString());
-
-
+        try {
+            Person person = new Person(name, lastName, age);
+            System.out.println(person.toString());
+        } catch (NameUndefinedException | IncorrectAgeException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
